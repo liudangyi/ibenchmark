f09ef822f508061acf3767362ee89a8c89c6bb2c
Dangyi Liu <leedy.liu@gmail.com>
2017-05-17 19:28:36 -0700

    File-backed mmap

--- src/memory/pagefault.h
+++ src/memory/pagefault.h
@@ -3,13 +3,15 @@
 #include <errno.h>
 
 BEGIN_TEST_PREP(mem_pagefault)
+    // a big file
+    int *fd = open("/var/db/dyld/dyld_shared_cache_x86_64h", O_RDONLY);
     size_t size = 256 << 20; // 256 MB
-    uint8_t *data = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, 0, 0);
+    volatile uint8_t *data = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
     if (errno) {
       fprintf(stderr, "mmap: %s\n", strerror(errno));
       exit(-1);
     }
 BEGIN_TEST_LOOP((size >> 12) / 2) // page size, divided by 2 since we will time times here
-    data[i << 12] = 0x80;
+    data[i << 12];
 END_TEST_LOOP
 END_TEST_PREP
